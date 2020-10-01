2520029accd88009a4298e87f169cdc863d35509
Neo <31198370+GuidoTapia@users.noreply.github.com>
2020-10-01 13:08:20 -0500

    Update producerConsumer.cpp

--- 01-10/producerConsumer.cpp
+++ 01-10/producerConsumer.cpp
@@ -5,13 +5,10 @@
 
 #define MSG_MAX 50
 
-/*Nro threads*/
 int n_threads = 1;
 
-/*Array global de semaforos*/
 sem_t* semaphores;
 char** messages;
-
 
 void* Send_msg(void* rank){
     long my_rank = (long)rank;
@@ -32,31 +29,20 @@ void* Send_msg(void* rank){
 }
 
 int main(int argc, char* argv[]){
-    /*Obtener el número de threads*/
     n_threads = atoi(argv[1]);
-
-    /*Inicializar array de threads*/
     pthread_t* my_threads = new pthread_t[n_threads];
-
-    /*Inicializar el array de semaforas*/
     semaphores = new sem_t[n_threads];
-
-    /*Inicializar buffers de mensajes*/
     messages = new char*[n_threads];
     for(int i=0;i<n_threads;i++){
         messages[i] = new char[MSG_MAX];
     }
-
     for(long thread=0;thread<n_threads;thread++){
         pthread_create(&my_threads[thread],NULL, Send_msg, (void*)thread);
     }
-
     for(long i=0;i<n_threads;i++){
         pthread_join(my_threads[i], NULL);
     }
-
-
-    /*Liberar memoria*/
+    
     delete[] semaphores;
 
     for(int i=0;i<n_threads;i++){
